98f165d4eeb49bb0b46d07aa3a3eb941111db448
Tom Brooks <tombrooks@Toms-MacBook-Pro.local>
2018-12-31 13:22:59 -0800

    Add rails 3.2.18

---
+++ fresh_3_2_18/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fresh3218::Application.config.secret_token = '134843204acdfe2cf1014383770f2fc84b2923f1fa72e2a9389ab0951847c744de1aab0982b22cb5c8a08c84506848b963ed7e536dd279278dd6fddf967d94cc'
---
+++ fresh_3_2_18/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fresh3218::Application.config.session_store :cookie_store, key: '_fresh_3_2_18_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fresh3218::Application.config.session_store :active_record_store
